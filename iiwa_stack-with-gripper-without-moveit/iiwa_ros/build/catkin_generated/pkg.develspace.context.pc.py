3353b88633f0c90fae1f648843fc525358ce95f5
xiao-xiao-jiang <193355797@qq.com>
2022-05-21 16:27:11 +0800

    调整目录

--- iiwa_stack-with-gripper-without-moveit/iiwa_ros/build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/abc418/my_iiwa_stack_latest/src/iiwa_stack-update-v1.7-from-upstream/iiwa_ros/include".split(';') if "/home/abc418/my_iiwa_stack_latest/src/iiwa_stack-update-v1.7-from-upstream/iiwa_ros/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "iiwa_msgs;roscpp".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-liiwa_ros".split(';') if "-liiwa_ros" != "" else []
-PROJECT_NAME = "iiwa_ros"
-PROJECT_SPACE_DIR = "/home/abc418/my_iiwa_stack_latest/src/iiwa_stack-update-v1.7-from-upstream/iiwa_ros/build/devel"
-PROJECT_VERSION = "1.4.0"
--- iiwa_stack-with-gripper-without-moveit/iiwa_ros/build/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/abc418/my_iiwa_stack_latest/src/iiwa_stack-update-v1.7-from-upstream/iiwa_ros/build/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
